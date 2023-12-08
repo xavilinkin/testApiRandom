1b17ff126b0c7c0f2849410ec2fe21761c8d9e45
xavilinkin <linkinxavi@gmail.com>
2023-12-08 20:28:39 +0100

    retrofit: add viewmodel and test call api randomusers

--- app/src/main/java/com/xavi/testapirandom/ui/view/fragments/ListRandomFragment.kt
+++ app/src/main/java/com/xavi/testapirandom/ui/view/fragments/ListRandomFragment.kt
@@ -5,15 +5,20 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import com.xavi.testapirandom.databinding.FragmentListRandomBinding
+import com.xavi.testapirandom.ui.viewmodel.ListRandomViewModel
 
 class ListRandomFragment : Fragment() {
     private var _binding: FragmentListRandomBinding? = null
     private val binding get() = _binding!!
 
+    private val lisRandomViewModel: ListRandomViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        lisRandomViewModel.onCreate("1")
     }
 
     override fun onCreateView(
@@ -22,6 +27,7 @@ class ListRandomFragment : Fragment() {
     ): View? {
         _binding = FragmentListRandomBinding.inflate(inflater, container, false)
         testClick(binding)
+        testGetRandomUsers()
         return binding.root
     }
 
@@ -29,6 +35,21 @@ class ListRandomFragment : Fragment() {
         binding.test1.setOnClickListener {
             val passArgs = ListRandomFragmentDirections.actionListRandomFragmentToDetailRandomFragment("Hola")
             findNavController().navigate(passArgs)
+        }
+    }
+
+    private fun testGetRandomUsers() {
+        lisRandomViewModel.getListRandomUsers().observe(
+            viewLifecycleOwner
+        ) { result ->
+            result?.let { usersResult ->
+                val names = buildString {
+                    usersResult.results?.forEach { user ->
+                        append(user.name.first)
+                    }
+                }
+                binding.test1.text = names
+            }
         }
     }
 
---
+++ app/src/main/java/com/xavi/testapirandom/ui/viewmodel/ListRandomViewModel.kt
@@ -0,0 +1,21 @@
+package com.xavi.testapirandom.ui.viewmodel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.xavi.testapirandom.data.model.RandomModel
+import com.xavi.testapirandom.domain.GetRandomUsersCase
+import kotlinx.coroutines.launch
+
+class ListRandomViewModel: ViewModel() {
+    private var getRandomUsers = GetRandomUsersCase()
+    private val listRandomUsers = MutableLiveData<RandomModel?>()
+    fun getListRandomUsers(): LiveData<RandomModel?> = listRandomUsers
+
+    fun onCreate(page: String) {
+        viewModelScope.launch {
+            listRandomUsers.postValue(getRandomUsers(page))
+        }
+    }
+}
